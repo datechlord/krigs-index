edd2dfe90f14cc653d89373c48f38fae18251715
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-03-04 05:58:49 +0800

    Update MainLandingPage.js

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -23,15 +23,14 @@ export default () => {
   const imageCss = tw`rounded-4xl`;
   return (
     <AnimationRevealPage>
-      <Hero
-        heading={<>Delicious & Affordable <HighlightedText>Meals Near You.</HighlightedText></>}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        imageSrc="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
-        imageCss={imageCss}
-        imageDecoratorBlob={true}
-        primaryButtonText="Order Now"
-        watchVideoButtonText="Meet The Chefs"
-      />
+    <Hero
+      heading={<>Building the Future of Digital Asset <HighlightedText> Insurance</HighlightedText></>}
+      description="A decentralized P2P insurance marketplace with easy claims and instant payouts. Powered by Polkadot."
+      imageSrc="https://insured.finance/wp-content/uploads/2020/12/insured-hero-image.svg"
+      imageCss={imageCss}
+      imageDecoratorBlob={true}
+      primaryButtonText="join"
+    />
       <MainFeature
         subheading={<Subheading>Established Since 2014</Subheading>}
         heading={
