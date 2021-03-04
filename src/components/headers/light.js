db3c7cc66de0439f2b45ef83a195ee3e4815cbf1
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-03-04 15:19:42 +0800

    lightupdate

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -72,14 +72,11 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
    */
   const defaultLinks = [
     <NavLinks key={1}>
+      <NavLink href="/#">Home</NavLink>
       <NavLink href="/#">About</NavLink>
+      <NavLink href="/#">Features</NavLink>
       <NavLink href="/#">Blog</NavLink>
-      <NavLink href="/#">Pricing</NavLink>
-      <NavLink href="/#">Contact Us</NavLink>
-      <NavLink href="/#" tw="lg:ml-12!">
-        Login
-      </NavLink>
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Sign Up</PrimaryLink>
+      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Telegram</PrimaryLink>
     </NavLinks>
   ];
 
