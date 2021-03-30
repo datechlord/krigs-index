660fb5ef0f95c554e3d665b12fa3eeadb2f30bb3
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-03-31 03:01:22 +0800

    Update MainLandingPage.js

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -18,9 +18,49 @@ export default () => {
       imageDecoratorBlob={true}
       primaryButtonText="Read The Docs"
     />
+    <FAQ
+        subheading={<Subheading>FAQS</Subheading>}
+        heading={
+          <>
+            You have <HighlightedText>Questions ?</HighlightedText>
+          </>
+        }
+        faqs={[
+          {
+            question: "Are all the templates easily customizable ?",
+            answer:
+              "Yes, they all are. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "How long do you usually support an standalone template for ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "What kind of payment methods do you accept ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "Is there a subscribption service to get the latest templates ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "Are the templates compatible with the React ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          },
+          {
+            question: "Do you really support Internet Explorer 11 ?",
+            answer:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          }
+        ]}
+      />
+      <Footer />
     </AnimationRevealPage>
-    <FAQ />
-    <Footer />
+    
   );
   
 }
