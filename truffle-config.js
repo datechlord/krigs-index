263748b3824266b5e79ac0a8301fe13174c81499
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-04-01 04:32:01 +0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -1,28 +1,8 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const provider = new HDWalletProvider({
+  privateKeys: ['39d0ba91a16af000fceb90aad1bbb358a2f2de62765c9438355386591a9b099b'],
+  providerOrUrl: 'https://data-seed-prebsc-2-s2.binance.org:8545/'
+});
 
 module.exports = {
   /**
@@ -35,18 +15,12 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+     networks: {
+      binanceTestnet: {
+      provider: () => provider,
+      network_id: "97", 
+      gas: 3000000
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
