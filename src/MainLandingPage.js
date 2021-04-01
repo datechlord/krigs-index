5f94c0414a9dd3871c8521c8e15c66d6979f01f7
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-04-01 23:42:21 +0800

    Update MainLandingPage.js

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -14,59 +14,50 @@ export default () => {
     const imageCss = tw`rounded-4xl`;
     const items = [{
       title: "Q4 2020",
-      cardTitle: "Q4",
-      cardSubtitle:"Equity Funding, Going Public, Token-Private Sale",
+      cardTitle: "Equity Funding",
       cardDetailedText: "Equity Funding, Going Public, Token-Private Sale",
     },
     {
       title: "Q1 2021",
-      cardTitle: "Q1",
-      cardSubtitle:"PR and Community Building, Token Launch, Listing on Exchanges, Product Development, Testing, First Beta Desktop Version",
+      cardTitle: "PR and Community Building",
       cardDetailedText: "PR and Community Building, Token Launch, Listing on Exchanges, Product Development, Testing, First Beta Desktop Version",
     },
     {
       title: "Q2 2021",
-      cardTitle: "Q2",
-      cardSubtitle:"Beta to Live Version Desktop, Beta App IOS, Extended Structured Products, Beta Rating System",
+      cardTitle: "Beta to Live Version Desktop",
       cardDetailedText: "Beta to Live Version Desktop, Beta App IOS, Extended Structured Products, Beta Rating System",
     },
     {
       title: "Q3 2021",
-      cardTitle: "Q3",
-      cardSubtitle:"Multi-Collateral Escrow, Shift to DAO Governance, Fixed Liquidity Provider Structure",
+      cardTitle: "Multi-Collateral Escrow",
       cardDetailedText: "Multi-Collateral Escrow, Shift to DAO Governance, Fixed Liquidity Provider Structure",
     },
     {
       title: "Q4 2021",
-      cardTitle: "Q4",
-      cardSubtitle:"Enough Instant Liquidity for major assets, P2P Insurance for Re-Insurance",
+      cardTitle: "Enough Instant Liquidity for major assets",
       cardDetailedText: "Enough Instant Liquidity for major assets, P2P Insurance for Re-Insurance",
     },
     {
       title: "Q1 2022",
-      cardTitle: "Q1",
-      cardSubtitle:"Liquidity Mining, Live Protocol Rating System, Live Exchange Rating System, Live User Rating System.",
+      cardTitle: "Liquidity Mining, Live Protocol Rating System",
       cardDetailedText: "Liquidity Mining, Live Protocol Rating System, Live Exchange Rating System, Live User Rating System.",
     },
 
     {
       title: "Q2 2022",
-      cardTitle: "Q2",
-      cardSubtitle:"System Integration with Exchanges, System Integration with Trading Terminals.",
+      cardTitle: "System Integration with Exchanges",
       cardDetailedText: "System Integration with Exchanges, System Integration with Trading Terminals",
     },
 
     {
       title: "Q3 2022",
-      cardTitle: "Q3",
-      cardSubtitle:"Safe and Krigs Purchase of Crypto Assets with Fiat, Krigs Crypto Debit Card",
+      cardTitle: "Safe and Krigs Purchase of Crypto Assets",
       cardDetailedText: "Safe and krigs Purchase of Crypto Assets with Fiat, Insured Crypto Debit Card",
     },
 
     {
       title: "Q4 2022",
-      cardTitle: "Q4",
-      cardSubtitle:"Safe and Insured Purchase of Crypto Assets with Fiat, Insured Crypto Debit Card",
+      cardTitle: "Safe and Insured Purchase of Crypto Assets with Fiat",
       cardDetailedText: "Safe and Insured Purchase of Crypto Assets with Fiat, Insured Crypto Debit Card",
     },
     ];
