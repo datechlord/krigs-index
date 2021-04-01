cc3eeca8abbbe8730c7e94b47d62dd60c10c2af3
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-04-01 19:25:24 +0800

    update

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -6,23 +6,8 @@ import MainFeature from "components/features/TwoColWithButton.js";
 import Footer from "components/footers/MiniCenteredFooter.js";
 import FeatureStats from "components/features/ThreeColCenteredStatsPrimaryBackground.js";
 import GetStarted from "components/cta/GetStartedLight.js";
-import { Chrono } from "react-chrono";
+import RoadMap from "component/roadmap/RoadMap.js";
 
-
-const Home = () => {
-    const items = [{
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-      media: {
-        type: "IMAGE",
-        source: {
-          url: "http://someurl/image.jpg"
-        }
-      }
-    }, 
-    ];
 
 export default () => {
   const HighlightedText = tw.span`bg-primary-500 text-gray-100 px-4 transform -skew-x-12 inline-block`;
@@ -38,9 +23,7 @@ export default () => {
       primaryButtonText="Read The Docs"
     />
     <FeatureStats/>
-    <div style={{ width: "500px", height: "400px" }}>
-        <Chrono items={items} />
-      </div>
+     <RoadMap/>
         <MainFeature />
       <GetStarted/>
       <Footer />
