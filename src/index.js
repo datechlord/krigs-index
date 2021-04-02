964ebce565f16bb434477674e99a147fe2105fa0
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-04-02 16:03:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Login from "./Login";
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
 ReactDOM.render(
   <App />,
-  <Login />,
   document.getElementById("root")
 );
