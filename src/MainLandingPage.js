0276c885df63925ec4e62c5f76a181e022a07b71
TokenDeFi <60732844+Tokenfinance@users.noreply.github.com>
2021-04-11 01:11:38 +0800

    Update MainLandingPage.js

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -9,6 +9,8 @@ import FAQ from "components/faqs/SimpleWithSideImage.js";
 import customerSupportIllustrationSrc from "images/customer-support-illustration.svg";
 import FeatureWithSteps from "components/features/TwoColWithSteps.js";
 import macHeroScreenshotImageSrc from "images/hero-screenshot-2.png";
+import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
+
 
 
 
@@ -42,6 +44,7 @@ export default () => {
         decoratorBlobCss={tw`xl:w-40 xl:h-40 opacity-15 -translate-x-1/2 left-1/2`}
       />
          <FeatureStats/>
+         <Blog />
      <FAQ
       imageSrc={customerSupportIllustrationSrc}
       imageContain={true}
