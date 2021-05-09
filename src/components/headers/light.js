9ff6778e67e0185b6f3c5710cb1247c65c1037dc
TokenDeFi <79978961+krigsfinance@users.noreply.github.com>
2021-05-09 17:18:58 +0800

    Update light.js

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -86,7 +86,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
   const defaultLogoLink = (
     <LogoLink href="/">
       <img src={logo} alt="logo" />
-      KRIGS Finance
+      Krigs Finance
     </LogoLink>
   );
 
