591ebc57bfb8653e7621a80709d74fd1d776902d
TokenDeFi <79978961+krigsfinance@users.noreply.github.com>
2021-05-12 03:36:25 +0800

    Update ThreeColCenteredStatsPrimaryBackground-2.js

--- src/components/features/ThreeColCenteredStatsPrimaryBackground-2.js
+++ src/components/features/ThreeColCenteredStatsPrimaryBackground-2.js
@@ -9,7 +9,7 @@ const Container = tw(ContainerBase)`my-8 lg:my-10 text-gray-100 -mx-8 px-8`;
 const HeadingContainer = tw.div``;
 const Heading = tw(SectionHeading)`sm:text-3xl bg-primary-800 md:text-4xl lg:text-5xl`;
 const Subheading = tw(SubheadingBase)`text-primary-100 text-right`;
-const Description = tw(SectionDescription)`text-primary-400 text-center mx-auto max-w-screen-md`;
+const Description = tw(SectionDescription)`text-primary-400 text-justify mx-auto max-w-screen-md`;
 
 export default ({
   subheading = "Buy and Hold KRIGS",
