bb2064b955a63198cb35a3de4a1f13949d48cbfb
TokenDeFi <79978961+krigsfinance@users.noreply.github.com>
2021-05-16 12:03:22 +0800

    Update light.js

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -72,6 +72,7 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
    */
   const defaultLinks = [
     <NavLinks key={1}>
+      <NavLink href="/#" target="blank">NFT Marketplace</NavLink>
       <NavLink href="https://bscscan.com/token/0xfa90a78e63e9bfe95c1270d8be0cc36c2fe6c2c7">Contract</NavLink>
       <NavLink href="https://dex.guru/token/0xfa90a78e63e9bfe95c1270d8be0cc36c2fe6c2c7-bsc" target="blank">Live Chart</NavLink>
       <NavLink href="https://krigsfinance.medium.com/" target="blank">Blog</NavLink>
